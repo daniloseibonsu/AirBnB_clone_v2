413e49f51ca31a5473d8a3d87f5f6bc6b156c33d
daniloseibonsu <daniloseibonsu@gmail.com>
2024-04-07 11:10:22 -0700

    AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-"""distributes an archive to the coding-max.tech web servers,
-   using the function do_deploy"""
+"""
+    Distributes an archive to your web servers,
+    using the function do_deploy
+    def do_deploy(archive_path):
+    Return False iff archive path doesn't exist
+"""
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['web-01.coding-max.tech', 'web-02.coding-max.tech']
+env.hosts = ['3.229.113.167', '3.234.210.158']
+env.user = 'ubuntu'
+env.identity = '~/.ssh/school'
+env.password = None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the coding-max.tech web servers"""
+    """
+    Deploys an archive to a server
+    """
     if exists(archive_path) is False:
         return False
-    file_name = archive_path.split("/")[-1]
-    file_wout = file_name.split(".")[0]
-    path = "/data/web_static/releases/"
     try:
+        file_N = archive_path.split("/")[-1]
+        n = file_N.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_wout))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_wout))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_wout))
-        run('rm -rf {}{}/web_static'.format(path, file_wout))
+        run('mkdir -p {}{}/'.format(path, n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_N, path, n))
+        run('rm /tmp/{}'.format(file_N))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, n))
+        run('rm -rf {}{}/web_static'.format(path, n))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_wout)) 
+        run('ln -s {}{}/ /data/web_static/current'.format(path, n))
+        run('chmod -R 755 /data/')
+        print("New version deployed!")
         return True
-    except:
+    except FileNotFoundError:
         return False
